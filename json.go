65f2b8ae289b9eea974e26ef6901017203e56bbc
Martin Garton <martin.garton@ft.com>
2015-08-25 08:41:05 +0100

    Faster json serialisation
    Faster json serialisation based on code from github.com/m4rw3r/uuid

--- json.go
+++ json.go
@@ -10,7 +10,32 @@ func (u UUID) MarshalJSON() ([]byte, error) {
 	if u == emptyUUID {
 		return []byte(`""`), nil
 	}
-	return []byte(`"` + u.String() + `"`), nil
+
+	b := [38]byte{}
+
+	for i, n := range []int{
+		1, 3, 5, 7,
+		10, 12,
+		15, 17,
+		20, 22,
+		25, 27, 29, 31, 33, 35,
+	} {
+		b[n] = halfbyte2hexchar[(u[i]>>4)&0x0f]
+		b[n+1] = halfbyte2hexchar[u[i]&0x0f]
+	}
+
+	b[0] = '"'
+	b[9] = '-'
+	b[14] = '-'
+	b[19] = '-'
+	b[24] = '-'
+	b[37] = '"'
+
+	return b[:], nil
+}
+
+var halfbyte2hexchar = []byte{
+	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102,
 }
 
 func (u *UUID) UnmarshalJSON(data []byte) error {
